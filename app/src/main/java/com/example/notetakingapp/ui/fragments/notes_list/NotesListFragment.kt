4e3b3567974ec70bdfda7b02f6e3d495e81aa8d4
Sukhdip <=>
2021-02-17 23:19:37 -0800

    created notes list, add note, and update note fragments and their ui

---
+++ app/src/main/java/com/example/notetakingapp/ui/fragments/notes_list/NotesListFragment.kt
@@ -0,0 +1,18 @@
+package com.example.notetakingapp.ui.fragments.notes_list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.notetakingapp.R
+
+class NotesListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes_list, container, false)
+    }
+}
